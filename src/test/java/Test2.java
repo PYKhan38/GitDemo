10262180bdd2f2d4960f7f62bbb30aa889376d08
PYKhan38 <patanyousafkhan38@gmail.com>
2023-06-06 16:59:06 +0530

    add updated files in develop

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -4,7 +4,16 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is done by architect in develop branch of GitDemo");
-
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");
+		System.out.println("This is done by architect in develop branch of GitDemo");	
+		
 	}
 
 }
