25006652c69fed783f4436523b8f331686770e2e
PYKhan38 <patanyousafkhan38@gmail.com>
2023-06-06 16:17:02 +0530

    This is committed from GitX

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,5 +6,12 @@ public class Test {
 		System.out.println("This is my first update");
 		System.out.println("This is my second update");
 		System.out.println("This is my third update");
+		System.out.println("This is my forth update");
+		System.out.println("This is my fifth update");
+		System.out.println("This is my sixth update");
+		System.out.println("This is my seventh update");
+		System.out.println("This is my eight update");
+		System.out.println("This is my nine update");
+		System.out.println("This is my ten update");
 	}
 }
